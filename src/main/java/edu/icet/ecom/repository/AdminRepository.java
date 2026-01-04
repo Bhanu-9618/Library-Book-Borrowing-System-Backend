2ced2567ce030d1bd5eb7242018fe2a633064fcd
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2026-01-04 22:30:10 +0530

    Implement Login function

---
+++ src/main/java/edu/icet/ecom/repository/AdminRepository.java
@@ -0,0 +1,11 @@
+package edu.icet.ecom.repository;
+
+import edu.icet.ecom.model.entity.AdminEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface AdminRepository extends JpaRepository<AdminEntity, Long> {
+    Optional<AdminEntity> findByEmailAndPassword(String email, String password);
+}
---
+++ src/main/java/edu/icet/ecom/service/Impl/LoginServiceImpl.java
@@ -0,0 +1,33 @@
+package edu.icet.ecom.service.Impl;
+
+import edu.icet.ecom.model.dto.LoginDto;
+import edu.icet.ecom.model.entity.AdminEntity;
+import edu.icet.ecom.repository.AdminRepository;
+import edu.icet.ecom.service.LoginService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+    @Autowired
+    AdminRepository adminRepository;
+
+    @Override
+    public boolean authenticate(LoginDto loginDto) {
+        return adminRepository.findByEmailAndPassword(
+                loginDto.getEmail(),
+                loginDto.getPassword()
+        ).isPresent();
+    }
+
+    @PostConstruct
+    public void initAdmin() {
+        if (adminRepository.count() == 0) {
+            AdminEntity admin = new AdminEntity();
+            admin.setEmail("admin@gmail.com");
+            admin.setPassword("admin123");
+            adminRepository.save(admin);
+        }
+    }
+}

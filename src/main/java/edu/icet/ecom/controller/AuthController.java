92615f40acaf163c37d432b009c4c1ad41870399
Nipun <nipunbhanuka12@gmail.com>
2026-03-18 13:20:06 +0530

    feat: implement AuthController to handle login and JWT issuance

---
+++ src/main/java/edu/icet/ecom/controller/AuthController.java
@@ -0,0 +1,46 @@
+package edu.icet.ecom.controller;
+
+import edu.icet.ecom.model.dto.LoginDto;
+import edu.icet.ecom.security.JwtUtils;
+import edu.icet.ecom.security.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginDto loginDto) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        String jwt = jwtUtils.generateToken(userDetails.getUsername());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("token", jwt);
+        response.put("id", userDetails.getId());
+        response.put("username", userDetails.getUsername());
+
+        return ResponseEntity.ok(response);
+    }
+}

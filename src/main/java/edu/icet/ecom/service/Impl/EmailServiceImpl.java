bd5ce8be0443cebfca354db419ca8cfe3d53131b
Nipun <nipunbhanuka12@gmail.com>
2026-04-04 21:34:55 +0530

    make email sending async to prevent API blocking and 500 errors

--- src/main/java/edu/icet/ecom/service/Impl/EmailServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/Impl/EmailServiceImpl.java
@@ -1,13 +1,17 @@
 package edu.icet.ecom.service.Impl;
 
 import edu.icet.ecom.service.EmailService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
 @Service
+@Slf4j
 public class EmailServiceImpl implements EmailService {
 
     @Autowired
@@ -18,11 +22,21 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendSimpleMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromEmail);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        emailSender.send(message);
+        // Run the email sending asynchronously so it doesn't block the UI
+        CompletableFuture.runAsync(() -> {
+            try {
+                SimpleMailMessage message = new SimpleMailMessage();
+                message.setFrom(fromEmail);
+                message.setTo(to);
+                message.setSubject(subject);
+                message.setText(text);
+                emailSender.send(message);
+                log.info("Email sent successfully to {}", to);
+            } catch (Exception e) {
+                // Catch any exception (like timeout due to Render blocking SMTP)
+                // This prevents a 500 Internal Server Error from ever reaching the user
+                log.error("Failed to send email to {}. Error: {}", to, e.getMessage());
+            }
+        });
     }
 }
